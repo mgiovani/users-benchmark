3f95dd96ef39da5042a3d8b17fd210453a14a763
Giovani Moutinho <giovanim@ymail.com>
2021-10-28 14:20:51 -0300

    Add postgresql connection

--- users_benchmark/settings.py
+++ users_benchmark/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'users_benchmark.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'users_benchmark',
+        'USER': 'postgres',
+        'PASSWORD': 'abc123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
